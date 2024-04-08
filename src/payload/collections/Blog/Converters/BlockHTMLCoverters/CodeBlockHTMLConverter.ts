42d71f77b1cd67ca856cf0e6281619d535fad9a4
Martin George <marty@dingo.foo>
2024-04-08 16:26:15 +0000

    mermaid stuff

--- src/payload/collections/Blog/Converters/BlockHTMLCoverters/CodeBlockHTMLConverter.ts
+++ src/payload/collections/Blog/Converters/BlockHTMLCoverters/CodeBlockHTMLConverter.ts
@@ -1,19 +1,18 @@
 import fs from 'fs'
+import { codeToHtml } from 'shiki'
 
 export const CodeBlockHTMLConverter: any = {
   converter: async ({ fields }) => {
-    // const { getHighlighter } = await import("shiki")
     // const theme = JSON.parse(fs.readFileSync('@/styles/themes/shiki/greyscale.json', 'utf8'))
     // const highlighter = await getHighlighter({
-    //     themes: [theme],
-    //     langs: []
+    //   themes: [theme],
+    //   langs: [],
     // })
-    // const html = highlighter.codeToHtml(fields.data.codeContent, {
-    //     lang: fields.data.codeLanguage,
-    //     theme: 'greyscale'
-    // })
-    // return html
-    return fields.codeLanguage
+    const html = codeToHtml(fields.data.codeContent, {
+      lang: fields.data.codeLanguage,
+      theme: 'solarized-dark',
+    })
+    return html
   },
   nodeTypes: ['block'],
 }
