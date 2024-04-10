e1d22580243f2d7b75c6f941710973db10b10e5c
Martin George <marty@dingo.foo>
2024-04-10 14:48:50 +0000

    commit

--- src/components/ui/mermaid/Mermaid/ViewMermaid.tsx
+++ src/components/ui/mermaid/Mermaid/ViewMermaid.tsx
@@ -1,31 +1,34 @@
 'use client'
 
-import React from "react"
+import React from 'react'
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/shadcn-ui/dialog"
-import { Button } from "@/components/lib/shadcn-ui/button"
-import { ZoomInIcon } from "@radix-ui/react-icons"
-import "./mermaid.css"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/shadcn-ui/dialog'
+import { Button } from '@/components/lib/shadcn-ui/button'
+import { ZoomInIcon } from '@radix-ui/react-icons'
+import './mermaid.css'
 
-export default function ViewMermaid({ mermaidSvg }: { mermaidSvg: string }){
-    return (
-        <Dialog>
-            <DialogTrigger>
-                <Button variant={"ghost"} size={"sm"}>
-                    <ZoomInIcon className="h-6 w-6 mt-0.5 text-sm" /> 
-                    <p>View Diagram</p>
-                </Button>
-            </DialogTrigger>
-                <DialogContent className="max-w-none max-h-fit h-[95vh] w-[95vw] prose dark:prose-invert">
-
-                <div dangerouslySetInnerHTML={{ __html: mermaidSvg }} />
-            </DialogContent>
-            </Dialog>
-    )
+export default function ViewMermaid({ mermaidSvg }: { mermaidSvg: string }) {
+  return (
+    <div className="text-center">
+      <Dialog>
+        <DialogTrigger>
+          <div>
+            <Button variant={'ghost'} size={'sm'}>
+              <ZoomInIcon className="h-6 w-6 mt-0.5 text-sm" />
+              <p>View Diagram</p>
+            </Button>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="max-w-none max-h-fit h-[95vh] w-[95vw] prose dark:prose-invert overflow-auto">
+          <div dangerouslySetInnerHTML={{ __html: mermaidSvg }} />
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
 }
