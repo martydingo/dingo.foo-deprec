de2b5587b08a9a834df784c9853eec07cf277d1e
Martin George <marty@dingo.foo>
2024-04-10 14:50:05 +0000

    commit

--- src/payload/collections/Projects/Converters/BlockHTMLCoverters/ProjectMermaidDiagramBlockHTMLConverter.ts
+++ src/payload/collections/Projects/Converters/BlockHTMLCoverters/ProjectMermaidDiagramBlockHTMLConverter.ts
@@ -1,5 +1,7 @@
 import path from 'path'
 import fs from 'fs'
+import { mermaidThemeDirectives } from '@/styles/themes/mermaid/nightfox'
+import { mermaidFlowchartDirectives } from '@/styles/themes/mermaid/generic'
 
 export const ProjectMermaidDiagramBlockHTMLConverter = {
   converter: async ({ fields }) => {
@@ -7,7 +9,29 @@ export const ProjectMermaidDiagramBlockHTMLConverter = {
       .replace(/ /g, '_')
       .replace(/'/g, '_')
       .toLowerCase()}`
-    const encodedMermaidCode = btoa(fields.mermaidDiagramCode)
+    const mermaidCode = fields.mermaidDiagramCode as string
+    const mermaidLines = mermaidCode.split('\n')
+    let encodedMermaidCode = ''
+    if (mermaidLines[0].includes('%%{')) {
+      const mermaidDirectivesEndLine = mermaidLines.findIndex((line) => line.includes('}%%'))
+      const mermaidDirectives = JSON.parse(
+        mermaidLines.slice(1, mermaidDirectivesEndLine).join('\n').replace('init: ', ''),
+      )
+      mermaidDirectives.theme = 'base'
+      mermaidDirectives.themeVariables = Object.assign({}, mermaidThemeDirectives.themeVariables)
+      mermaidDirectives.flowchart = Object.assign(
+        mermaidFlowchartDirectives.flowchart,
+        mermaidDirectives.flowchart,
+      )
+      const mermaidDirectiveString = `%%{init:${JSON.stringify(mermaidDirectives)}}%%`
+      const mermaidCodeWithoutDirectives = mermaidLines
+        .slice(mermaidDirectivesEndLine + 1)
+        .join('\n')
+      encodedMermaidCode = btoa(`${mermaidDirectiveString}\n${mermaidCodeWithoutDirectives}`)
+    } else {
+      encodedMermaidCode = btoa(fields.mermaidCode)
+    }
+
     const mermaidFetchFQDN = `https://mermaid.ink/svg/${encodedMermaidCode}`
     const mermaidDiagram = await fetch(mermaidFetchFQDN).then((response) => {
       return response.text()
