d98e119be3bd537f4a42ceef4c6a9101c9828d41
Martin George <marty@dingopaws.net>
2024-04-12 14:08:22 +0100

    commit

--- src/payload/collections/Projects/Converters/BlockHTMLCoverters/CodeBlockHTMLConverter.ts
+++ src/payload/collections/Projects/Converters/BlockHTMLCoverters/CodeBlockHTMLConverter.ts
@@ -1,18 +1,18 @@
 //@ts-nocheck
 
 import fs from 'fs'
-import { codeToHtml } from 'shiki'
+import { getHighlighter } from 'shiki'
 
 export const CodeBlockHTMLConverter: any = {
   converter: async ({ fields }) => {
-    // const theme = JSON.parse(fs.readFileSync('@/styles/themes/shiki/greyscale.json', 'utf8'))
-    // const highlighter = await getHighlighter({
-    //   themes: [theme],
-    //   langs: [],
-    // })
-    const html = codeToHtml(fields.codeContent, {
+    const theme = JSON.parse(fs.readFileSync('@/styles/themes/shiki/halcyon.json', 'utf8'))
+    const highlighter = await getHighlighter({
+      themes: [theme],
+      langs: [fields.codeLanguage],
+    })
+    const html = highlighter.codeToHtml(fields.codeContent, {
       lang: fields.codeLanguage,
-      theme: 'nord',
+      theme: 'halcyon',
     })
     return html
   },
